1bed4f514f246b6000b1b6f23f5274cdb3f8357e
seanperfecto <seanperfecto@gmail.com>
2017-05-15 11:43:36 -0700

    Setup project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/melody.jsx',
+  output: {
+    filename: './app/assets/javascripts',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
