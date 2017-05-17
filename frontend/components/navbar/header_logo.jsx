1065c247830efe903ba457b7f03b0f9a532ed8f4
seanperfecto <seanperfecto@gmail.com>
2017-05-16 23:42:58 -0700

    Fix header

--- frontend/components/navbar/header_logo.jsx
+++ frontend/components/navbar/header_logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const HeaderLogo = () => (
   <hgroup className='header-logo'>
-    <Link className='header-link' to='/'><img height="30px" width="30px" src='/assets/melody_logo.png' alt="logo" /></Link>
-    <Link className='header-link' to='/'>Melody</Link>
+    <Link className='header-link header-link-name' to='/'>M E L O D Y</Link>
+    <Link className='header-link' to='/'><img height="55px" width="55px" src='/assets/melody_logo3.png' alt="logo" /></Link>
   </hgroup>
 );
