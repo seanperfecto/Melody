c435b0caef10b293d8d394b43c2c682844bb153f
seanperfecto <seanperfecto@gmail.com>
2017-05-19 14:14:23 -0700

    Fix square albums

--- frontend/components/navbar/header_logo.jsx
+++ frontend/components/navbar/header_logo.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 export const HeaderLogo = () => (
   <hgroup className='header-logo'>
-    <Link className='header-link header-link-name' to='/'>M E L O D Y</Link>
-    <Link className='header-link' to='/'><img height="45px" width="45px"
+    <Link className='header-link header-link-name' to='/discover'>M E L O D Y</Link>
+    <Link className='header-link' to='/discover'><img height="45px" width="45px"
       src='http://res.cloudinary.com/dqr2mejhc/image/upload/v1495044542/melody_logo3_q3qjg9.png'
       alt="logo" />
     </Link>
