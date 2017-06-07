5a0486f125b2478992d974b572eb4fe7919ed2c0
seanperfecto <seanperfecto@gmail.com>
2017-06-07 12:48:08 -0700

    Fix production bug

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,12 +3,12 @@ import { thunk } from '../middleware/thunk';
 import rootReducer from '../reducers/root_reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// Added this for production
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const createLogger = require('redux-logger');
-  thunk.push(createLogger());
-}
+// // Added this for production
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const createLogger = require('redux-logger');
+//   thunk.push(createLogger());
+// }
 
 const configureStore = (preloadedState = {}) => (
   createStore(
